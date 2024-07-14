9209ecd738522f21ceb11f3551109ea9c8523063
NANCHARAIAH JANNU <146073523+nanijannu614@users.noreply.github.com>
2024-07-14 11:56:56 +0530

    Create write a program to Print the no.of vowels in the string.py

---
+++ write a program to Print the no.of vowels in the string.py
@@ -0,0 +1,9 @@
+#Print the no.of vowels in the string 
+s=input()
+s2=s.lower()
+a=s2.count('a')
+e=s2.count('e')
+i=s2.count('i')
+o=s2.count('o') 
+u=s2.count('u')
+print (f'the number of vowels are:{a+e+i+o+u}')
