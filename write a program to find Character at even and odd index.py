ca6634abe2a26156543c78423f611e46b0db7e35
NANCHARAIAH JANNU <146073523+nanijannu614@users.noreply.github.com>
2024-07-14 11:58:42 +0530

    Create write a program to find Character at even and odd index.py

---
+++ write a program to find Character at even and odd index.py
@@ -0,0 +1,12 @@
+#Character at even and odd index
+s=input()
+i=0
+print('characters at even index')
+while i<len(s):
+    print(s[i])
+    i=i+2
+i=1
+print('characters at odd index')
+while i<len(s):
+    print(s[i])
+    i=i+2
